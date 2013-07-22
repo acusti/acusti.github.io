83c2f1fd10eaac35d8e4427cb272e29afd6086c0
Andrew Patton <andrew@acusti.ca>
2013-07-22 13:09:46 -0400

    Adding retina graphics helper

--- config.rb
+++ config.rb
@@ -20,3 +20,8 @@ relative_assets = true
 # line_comments = false
 
 preferred_syntax = :sass
+
+# Add retina compass helpers https://github.com/joelambert/Retina-Compass-Helpers
+retina_ext = File.join(File.dirname(__FILE__), "retina")
+require File.join(retina_ext, "lib", "sass_extensions.rb")
+add_import_path File.join(retina_ext, "stylesheets")
