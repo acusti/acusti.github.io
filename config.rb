46e3238a947e6f88ba2603ef5f41a7c94e926ef0
Andrew Patton <andrew@acusti.ca>
2013-07-22 13:08:55 -0400

    Converting CSS to SASS, Compass setup
    Restructuring CSS into collection of partials, adding basic imports,
    mixins, and variables based upon WP-Theme-Forme

---
+++ config.rb
@@ -0,0 +1,22 @@
+# Require any additional compass plugins here.
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "css"
+sass_dir = "sass"
+images_dir = "images"
+javascripts_dir = "js"
+
+# Development or production?
+environment = :development
+# environment = :production
+
+# Compress the CSS when in production
+output_style = environment == :production ? :compressed : :nested
+
+relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+# line_comments = false
+
+preferred_syntax = :sass
