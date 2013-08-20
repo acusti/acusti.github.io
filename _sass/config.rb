b836dfb4b840e96b85faa02647897fa98c03f75a
Andrew Patton <andrew@acusti.ca>
2013-08-20 15:56:30 -0400

    Fix CSS to SASS conversion, restructure SASS to use jekyll plugin

--- _sass/config.rb
+++ _sass/config.rb
@@ -2,10 +2,11 @@
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "css"
-sass_dir = "sass"
-images_dir = "images"
-javascripts_dir = "js"
+sass_dir = "./"
+css_dir = "../css"
+images_dir = "../images"
+javascripts_dir = "../js"
+relative_assets = true
 
 # Development or production?
 environment = :development
@@ -13,8 +14,6 @@ environment = :development
 
 # Compress the CSS when in production
 output_style = environment == :production ? :compressed : :nested
-
-relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 # line_comments = false
