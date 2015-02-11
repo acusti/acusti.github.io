0bf1166085076cc0e1fd3cbd3d4aeeedf3dd3ed1
Andrew Patton <andrew@acusti.ca>
2015-02-10 23:47:01 -0500

    Remove event manager / ready functions
    No need to keep them; if browser doesn’t support addEventListener, ton
    pis. Also, use standard `window.scrollY` property instead of
    `window.pageYOffset` alias

--- js/global.js
+++ js/global.js
@@ -1,111 +1,16 @@
-// General event handling
-// ----------------------
-
-// Rock solid addEvent() from http://www.dustindiaz.com/rock-solid-addevent/
-function addEvent( obj, type, fn ) {
-	if (obj.addEventListener) {
-		obj.addEventListener( type, fn, false );
-		EventCache.add(obj, type, fn);
-	}
-	else if (obj.attachEvent) {
-		obj["e"+type+fn] = fn;
-		obj[type+fn] = function() { obj["e"+type+fn]( window.event ); };
-		obj.attachEvent( "on"+type, obj[type+fn] );
-		EventCache.add(obj, type, fn);
-	}
-	else {
-		obj["on"+type] = obj["e"+type+fn];
-	}
-}
-
-var EventCache = function(){
-	var listEvents = [];
-	return {
-		listEvents : listEvents,
-		add : function(node, sEventName, fHandler){
-			listEvents.push(arguments);
-		},
-		flush : function(){
-			var i, item;
-			for(i = listEvents.length - 1; i >= 0; i = i - 1){
-				item = listEvents[i];
-				if(item[0].removeEventListener){
-					item[0].removeEventListener(item[1], item[2], item[3]);
-				}
-				if(item[1].substring(0, 2) != "on"){
-					item[1] = "on" + item[1];
-				}
-				if(item[0].detachEvent){
-					item[0].detachEvent(item[1], item[2]);
-				}
-				item[0][item[1]] = null;
-			}
-		}
-	};
-}();
-addEvent(window, 'unload', EventCache.flush);
-
-// A $(document).ready() type of a function
-// from http://stackoverflow.com/questions/799981/document-ready-equivalent-without-jquery
-// Relies on addEvent(), above
-var ready = (function () {
-	function ready(f) {
-		if (ready.done) return f();
-		if (ready.timer) {
-			ready.ready.push(f);
-		} else {
-			addEvent(window, 'load', isDOMReady);
-			ready.ready = [ f ];
-			ready.timer = setInterval(isDOMReady, 13);
-		}
-	}
-	function isDOMReady() {
-		if (ready.done) return false;
-		if (document && document.getElementsByTagName && document.getElementById && document.body) {
-			clearInterval(ready.timer);
-			ready.timer = null;
-			for (var i = 0; i < ready.ready.length; i++) {
-				ready.ready[i]();
-			}
-			ready.ready = null;
-			ready.done = true;
-		}
-	}
-	return ready;
-})();
-
-// Other helpers
-// -------------
-
-// Cross browser procedure to set CSS inline styles
-var setStyles = function(el, styles) {
-	if (typeof el.style.setAttribute === 'object') {
-		// Non-standard (IE 7)
-		el.style.setAttribute('cssText', styles);
-	} else {
-		// Standard
-		el.setAttribute('style', styles);
-	}
-};
-
-
-// End of global variables
-// Beginning of functionality
-
 (function() {
-	// On document ready
-	// -----------------
-	ready(function() {
+	// Poor man's document ready (this is last thing on page, so should work fine)
+	window.setTimeout(function() {
 		// Set up fixed nav (displayed under regular header and nav)
-		var nav = document.getElementsByClassName('top-nav');
-		if (nav.length) {
-			nav = nav[0].cloneNode(true);
+		var nav = document.querySelector('.top-nav');
+		if (nav !== null) {
+			nav = nav.cloneNode(true);
 			nav.className += ' ' + nav.className + '-fixed';
 			document.body.appendChild(nav);
 		}
 
 		// Fill in my email address (cheapo spam protection):
-		var emails = document.getElementsByClassName('email');
+		var emails = document.querySelectorAll('.email');
 		for (var i=0, len=emails.length; i<len; ++i) {
 			emails[i].href = 'mailto:andrew@acusti.ca' + '?subject=' + escape(document.title);
 			emails[i].innerHTML = 'andrew@acusti.ca';
@@ -119,16 +24,16 @@ var setStyles = function(el, styles) {
 		image_splash = document.querySelector('.post__splash > img');
 		if (image_splash !== null) {
 			imageParallax();
-			addEvent(window, 'scroll', imageParallax);
-			addEvent(window, 'resize', imageParallaxCalculate);
+			window.addEventListener('scroll', imageParallax);
+			window.addEventListener('resize', imageParallaxCalculate);
 		}
-	});
+	}, 1);
 
 	// Parallax effect (on scroll)
 	var image_splash,
 	    image_splash_wrap,
 	    parallax_speed = 0.3,
-	    pageYOffset_previous;
+	    scrollY_previous;
 
 	// Initializes parallax and implements it on scroll
 	// @uses imageParallaxCalculate
@@ -150,20 +55,20 @@ var setStyles = function(el, styles) {
 				image_splash_wrap.className += ' is-svg';
 			}
 			imageParallaxCalculate();
-			pageYOffset_previous = window.pageYOffset;
+			scrollY_previous = window.scrollY;
 		}
 
 		// Don't do any work if:
 		// 1. pageYOffset change is too small to matter
 		// 2. post__splash image isn't cropped
-		if (Math.abs(window.pageYOffset - pageYOffset_previous) * parallax_speed < 1.5 || image_splash.clientHeight - 20 < image_splash_wrap.clientHeight) {
+		if (Math.abs(window.scrollY - scrollY_previous) * parallax_speed < 1.5 || image_splash.clientHeight - 20 < image_splash_wrap.clientHeight) {
 			return;
 		}
 		// Cache pageYOffset
-		pageYOffset_previous = window.pageYOffset;
+		scrollY_previous = window.scrollY;
 
 		// Parallaxify
-		image_splash.style.bottom = Math.floor(pageYOffset_previous * parallax_speed * -1) + 'px';
+		image_splash.style.bottom = Math.floor(scrollY_previous * parallax_speed * -1) + 'px';
 	};
 
 	// Function to calculate dimensions and values for parallax
@@ -212,7 +117,7 @@ var setStyles = function(el, styles) {
 			comparison_image_wrap.className += ' image-comparison-wrap';
 			// Cache current content of toggle as 'data-text' attribute
 			comparison_toggles[i].setAttribute('data-text', comparison_toggles[i].innerHTML);
-			addEvent(comparison_toggles[i], 'click', toggleImageComparison);
+			comparison_toggles[i].addEventListener('click', toggleImageComparison);
 		}
 	};
 
@@ -235,5 +140,4 @@ var setStyles = function(el, styles) {
 			this.innerHTML = next_text;
 		}
 	};
-
 })();
