dd8a5c0782e065212c6359cf0b3e3d08e782982b
Andrew Patton <andrew@acusti.ca>
2025-01-15 12:22:09 -0800

    Refactor image-parallax.js to base it on img size
    • adjust parallax to always adjust image position to show all the way to the bottom of the image, regardless of device width and image dimensions
    • simplify implementation significantly

--- scripts/image-parallax.js
+++ scripts/image-parallax.js
@@ -3,7 +3,6 @@ import attachScrollFrame from 'onscrolling';
 // Parallax effect (on scroll)
 var image = null,
     image_wrap = null,
-    parallax_speed = 0.3,
     dimensions = {
         image: {},
         image_wrap: {},
@@ -14,85 +13,56 @@ var image = null,
     resizeTimeoutId;
 
 // Initializes parallax and implements it on scroll
-// @uses imageParallaxCalculate
 function imageParallax(update) {
-    scrollY = update.scrollY;
+    scrollY = update ? update.scrollY : window.scrollY;
     // Initialize
     if (image_wrap === null) {
-        // Adding our calculations to window load doesn't work when command clicking a post link to open it in a new tab in Chrome
-        // So instead, verify we have a usable image object and if not, use a timeout to check again in 150ms
+        // Verify we have a usable image object and if not, use a timeout to check again in 150ms
         if (image === null || !image.naturalWidth) {
-            window.setTimeout(imageParallax.bind(null, update), 150);
+            window.setTimeout(imageParallax, 150);
             return;
         }
-        image_wrap = image.parentElement;
 
+        image_wrap = image.parentElement;
         // Special case for svgs
         if (image.src.substring(image.src.length - 4) === '.svg') {
             image_wrap.classList.add('is-svg');
         }
-        imageParallaxCalculate();
+
         scrollY_previous = scrollY;
+
+        // Start listening for scroll events
+        if (detachScrollFrame) detachScrollFrame();
+        detachScrollFrame = attachScrollFrame(imageParallax);
     }
 
-    // Don't proceed (and don't cache scrolLY) if scrollY change is too small to matter
-    if (Math.abs(scrollY - scrollY_previous) * parallax_speed < 1.5) {
-        return;
-    }
+    var imageWrapHeight = image_wrap.clientHeight;
+    // Get approximate scroll percentage of splash image in viewport
+    var scrollPercentage = scrollY / imageWrapHeight;
+    if (scrollPercentage > 1) return;
+
+    // Possible translateY range is 0 to ½ of cropped height (because image is centered)
+    var maxTranslateY = (image.clientHeight - imageWrapHeight) / 2;
+
+    // Don't do any work if post__splash image isn't sufficiently cropped
+    if (maxTranslateY <= 10) return;
+
     // Cache scrollY
     scrollY_previous = scrollY;
 
-    // Don't do any work if post__splash image isn't cropped
-    if (image.clientHeight - 20 < image_wrap.clientHeight) return;
-
-    // Parallaxify
-    image.style.transform = 'translateY(' + Math.floor(scrollY_previous * parallax_speed * -1) + 'px' + ')';
-}
-
-// Function to calculate dimensions and values for parallax
-function imageParallaxCalculate() {
-    if (image && !image_wrap) {
-        image_wrap = image.parentElement;
-    }
-
-    if (image === null || image_wrap === null) {
-        // Remove onscroll listener
-        if (detachScrollFrame) detachScrollFrame();
-        return;
-    }
-    dimensions.image.height = image.clientHeight;
-
-    // Make sure we're listening
-    if (detachScrollFrame) detachScrollFrame();
-    detachScrollFrame = attachScrollFrame(imageParallax);
-
-    // Calculations
-    dimensions.image.naturalWidth = image.naturalWidth;
-    dimensions.image_wrap.width = image_wrap.clientWidth;
-
-    if (image.clientWidth < image_wrap.clientWidth) {
-        image_wrap.style.width = image.clientWidth + 'px';
-    } else {
-        image_wrap.style.width = '';
-        // If image is high-res (double resolution or thereabouts, set max-width at the smallest of clientHeight and full width * 0.5)
-        if (
-            dimensions.image.naturalWidth > 2100 &&
-            dimensions.image.naturalWidth / 2 < image_wrap.clientWidth
-        ) {
-            image.style.maxWidth = dimensions.image.naturalWidth / 2 + 'px';
-            image_wrap.style.width = dimensions.image.naturalWidth / 2 + 'px';
-        } else {
-            image.style.maxWidth = '';
-            image_wrap.style.width = '';
-        }
-    }
+    // Parallaxify by applying scroll percentage to translateY range
+    image.style.transform =
+        'translateY(' +
+        Math.min(Math.round(maxTranslateY * scrollPercentage * -1), maxTranslateY) +
+        'px' +
+        ')';
 }
 
 function onResizeDebouncer() {
     if (resizeTimeoutId) {
         window.clearTimeout(resizeTimeoutId);
     }
-    window.setTimeout(imageParallaxCalculate, 150);
+    window.setTimeout(imageParallax, 150);
 }
 
 // Return a function that initializes the effect
